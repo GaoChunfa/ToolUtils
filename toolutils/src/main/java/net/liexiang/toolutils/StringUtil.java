958eb2359072b59469b468a999bb1394fea0c267
明天你好 <gaochunfa1992@qq.com>
2020-11-04 10:26:39 +0800

    Initial commit

---
+++ toolutils/src/main/java/net/liexiang/toolutils/StringUtil.java
@@ -0,0 +1,47 @@
+
+package net.liexiang.toolutils;
+
+/**
+ * describe：处理字符工具类
+ * author： Gao Chunfa.
+ * time： 2017/2/28-10:41.
+ */
+public class StringUtil {
+
+    /**
+     * 比较两个字符串是否相等
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public static final boolean isEqual(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+        return str1.equals(str2);
+    }
+
+    /**
+     * 不为空
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isNotNull(String s) {
+        return s != null && !"".equals(s.trim());
+    }
+
+    /**
+     * 为空
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isNull(String s) {
+        return s == null || "".equals(s.trim());
+    }
+
+
+
+}
